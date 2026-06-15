7a0b03cf4e08420d53491cb2a131bdcd98c1f806
nightfury34 <mc.nightfury34@gmail.com>
2026-06-15 19:31:14 +0200

    add signals data

---
+++ data/signals.lua
@@ -0,0 +1,15 @@
+local signals = {}
+
+function signals.data()
+    return {
+        G55 = {
+            computerID = 212,
+            blocks = {
+                "11.1",
+                "11.2"
+            }
+        }
+    }
+end
+
+return signals
