54b2ec4bdc71912e435015cf42b746deb7828b82
nightfury34 <mc.nightfury34@gmail.com>
2026-06-15 22:34:28 +0200

    add new terminal command to set path

--- controller/signalController.lua
+++ controller/signalController.lua
@@ -2,6 +2,10 @@ local signals = require("data/signals")
 
 local signalController = {}
 
+function signalController.isSignal(signalId)
+    return signals[signalId] ~= nil
+end
+
 function signalController.getSignalBlocks(signalID)
     return signals.data()[signalID].blocks
 end
--- view/terminalView.lua
+++ view/terminalView.lua
@@ -1,3 +1,6 @@
+local signalController = require("controller/signalController")
+local pathController = require("controller/pathController")
+local reserveController = require("controller/reserveController")
 local terminalView = {}
 
 function terminalView.defaultTerminal()
@@ -9,6 +12,32 @@ function terminalView.defaultTerminal()
             print("Type in stuff and stuff will happen. I swear")
         elseif input == "exit" then
             break
+        elseif #input > 4 then
+            local startSignalID = input[1] .. input[2]
+            local targetSignalID = input[3] .. input[4]
+
+            if signalController.isSignal(startSignalID) then
+                if signalController.isSignal(targetSignalID) then
+
+                    local pathName = "" .. startSignalID .. targetSignalID
+
+                    if pathController.isPath(pathName) then
+                        -- set path
+                        if reserveController.isPathFree(pathName) then
+                            reserveController.reservePath(pathName)
+                            print("Set new path")
+                        else
+                            print("Path is already blocked")
+                        end
+                    else
+                        print("Not a valid Path")
+                    end
+                else
+                    print("Target Signal " .. targetSignalID .. " is not a valid Signal")
+                end
+            else
+                print("Start Signal " .. startSignalID .. " is not a valid Signal")
+            end
         end
     end
 end
